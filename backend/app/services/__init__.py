b3b8219e94692338bcded670b58590ec92932654
Ryan Vu <ryanvu1131@gmail.com>
2026-01-07 14:27:45 -0800

    comments

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,14 +1,22 @@
+import numpy as np
+import pandas as pd
+
 import joblib
 import logging
 
 class ModelService:
     def __init__(self, model_path: str):
-        self.classifier = None
+        """Initializaing model service by loading the model from the specified path"""
+        self.classifier = None # Set to none initially
         try:
-            self.classifier = joblib.load(model_path)
+            self.classifier = joblib.load(model_path) # Load model if we are able too
         except Exception as e:
+            # Warnings if no path found, but service can still run since classifier is None
             logging.warning("Model file not found. Service running without predictions.")
             print(f"Warning: Model file not found. {e}")
 
     def has_model(self):
+        """Check if the model is loaded successfully"""
         return self.classifier is not None
+    
+    
