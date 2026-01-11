f2d7159ff65443bc7980981febfe0e6ca0872187
Ryan Vu <ryanvu1131@gmail.com>
2026-01-11 00:59:15 -0800

    feedback

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -27,7 +27,7 @@ class ModelService:
         
         # Compute summary statistics - may change later
         stats = []
-        for col in target_columns:
+        for col in columns:
             if col in df.columns:
                 mu = df[col].mean()
                 sigma = df[col].std()
@@ -43,8 +43,8 @@ class ModelService:
                 
         features = stats # TODO: May add release_frame later (depends on how it is implemented elsewhere) 
         
-        # Flatten to numpy array
-        feature_array = stats.values.flatten().reshape(1, -1)
+        # numpy array
+        feature_array = np.array(stats).reshape(1, -1)
     
         return feature_array
         
@@ -71,3 +71,47 @@ class ModelService:
             "predicted_label": predicted_label
         }
     
+    def text_feedback(self, prediction, feature_array):
+        """Takes in dict and numpy feature array, returns text feedback based on prediction score"""
+        
+        score = prediction['score']
+        strengths = []
+        weaknesses = []
+        
+        
+        feats = feature_array[0] 
+        
+        # Getting information out
+        mean_elbow = feats[0]
+        std_shoulder  = feats[3]
+        std_wrist  = feats[5]
+        
+        # General Score Feedback
+        if score >= 80:
+            strengths.append("High consistency detected in your form.")
+        elif score >= 50:
+            strengths.append("There is room for improvement but getting there.")
+        else:
+            weaknesses.append("Overall mechanics inconsistent with training data.")
+            
+        # Elbow Feedback
+        if 80 <= mean_elbow <= 100:
+            strengths.append("Elbow angle is excellent.")
+        elif mean_elbow > 110:
+            weaknesses.append("Elbow is flaring out or too straight.")
+        elif mean_elbow < 70:
+            weaknesses.append("Elbow is bent too tight.")
+            
+        # Shoulder Feedback
+        if std_shoulder > 15:
+            weaknesses.append("Shoulder is unstable during the shot.")
+        else:
+            strengths.append("Upper arm stability is good.")
+            
+        # Wrist Feedback
+        if std_wrist < 5: 
+            weaknesses.append("Stiff wrist detected. Focus on flicking your wrist at release.")
+        elif std_wrist > 15:
+            strengths.append("Good follow-through and wrist flexion.")
+            
+        return strengths, weaknesses
